12d45461adaa481f8a395ad6359b30f1c8a3e819
babsdevi <personalforbabot@gmail.com>
2025-04-22 07:25:14 +0800

    Update Kryzstoffmain.py

--- Kryzstoffmain.py
+++ Kryzstoffmain.py
@@ -11,7 +11,7 @@ from rich.style import Style
 import pystyle
 from pystyle import Colors, Colorate
 
-from xenocpm import Tresehshs
+from Kryzstoff import babsdevi
 
 __CHANNEL_USERNAME__ = "KRYZSTOFF CPM1 TOOL GROUP"
 __GROUP_USERNAME__   = "KRYZSTOFF CPM1 TOOL CHANNEL"
@@ -139,7 +139,7 @@ if __name__ == "__main__":
         acc_password = prompt_valid_value("[bold][?] Account Password[/bold]", "Password", password=False)
         acc_access_key = prompt_valid_value("[bold][?] Access Key[/bold]", "Access Key", password=False)
         console.print("[bold cyan][%] Trying to Login[/bold cyan]: ", end=None)
-        cpm = Tresehshs(acc_access_key)
+        cpm = babsdevi(acc_access_key)
         login_response = cpm.login(acc_email, acc_password)
         if login_response != 0:
             if login_response == 100:
